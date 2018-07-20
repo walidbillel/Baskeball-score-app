b4e2c2127cc09d5d04c5e4d6526edbf8795f3213
willou <walid.boubguira@gmail.com>
2018-07-19 22:09:51 -0400

    some stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,11 @@
 
-var sportsdata_nba = require('sportsdata').NBA;
- 
-// Init the object with the access level, version, apikey, year, and season you care about
-sportsdata_nba.init('t', 1, apikey, '2012', 'REG');
- 
-// Get the season schedule
-sportsdata_nfl.getSeasonSchedule(function(error, schedule) {
- if (!error) {
-    console.log(schedule) // Print the season schedule for the NFL season
-  }
-});
+var nba = require('nba.js').stats;
+
+nba.teamGeneralStats(function(err, res) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  
+    console.log(res);
+  });
